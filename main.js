88f23bb9762bf95c707851f5063843d4f4358d38
Bread <113300714+UpBeat25@users.noreply.github.com>
2026-05-27 02:06:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+const pb = new PocketBase("http://127.0.0.1:4173");
+
+const options = [
+    "Engineering",
+    "Physics",
+    "Computer Science",
+    "Artificial Intelligence",
+    "Machine Learning",
+    "Robotics",
+    "Aerospace",
+    "Mathematics",
+    "Electronics",
+    "Mechanical Engineering",
+    "Civil Engineering",
+    "Quantum Computing",
+    "Astronomy",
+    "Data Science",
+    "Cybersecurity"
+];
